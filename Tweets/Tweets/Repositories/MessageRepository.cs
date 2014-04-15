8a883a10014c9daf1243c2b7c489720250bdcf6d
Chisamba <anfilatovd@mail.ru>
2014-04-15 20:01:18 +0600

    Update MessageRepository.cs

--- Tweets/Tweets/Repositories/MessageRepository.cs
+++ Tweets/Tweets/Repositories/MessageRepository.cs
@@ -1,8 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing.Design;
 using System.Linq;
+using System.Linq.Expressions;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver.Wrappers;
+using StructureMap.Query;
 using Tweets.ModelBuilding;
 using Tweets.Models;
 
@@ -25,32 +33,132 @@ namespace Tweets.Repositories
         public void Save(Message message)
         {
             var messageDocument = messageDocumentMapper.Map(message);
-            //TODO: Здесь нужно реализовать вставку сообщения в базу
+            messageDocument.Likes = new LikeDocument[0];
+            messagesCollection.Insert(messageDocument);
         }
 
         public void Like(Guid messageId, User user)
         {
-            var likeDocument = new LikeDocument {UserName = user.Name, CreateDate = DateTime.UtcNow};
-            //TODO: Здесь нужно реализовать вставку одобрения в базу
+            var find = messagesCollection.Find(
+                Query.And(Query<MessageDocument>.EQ(m => m.Id, messageId),
+                    Query<MessageDocument>.ElemMatch(l => l.Likes, c =>
+                        c.EQ(u => u.UserName, user.Name))));
+            if (find.Any())
+                return;
+            var likeDocument = new LikeDocument { UserName = user.Name, CreateDate = DateTime.UtcNow };
+            var search = Query<MessageDocument>.EQ(m => m.Id, messageId);
+            var updateQ = Update<MessageDocument>.Push(l => l.Likes, likeDocument);
+            messagesCollection.Update(search, updateQ);
+
         }
 
         public void Dislike(Guid messageId, User user)
         {
-            //TODO: Здесь нужно реализовать удаление одобрения из базы
+            var search = Query<MessageDocument>.EQ(m => m.Id, messageId);
+            var updateQ = Update<MessageDocument>.Pull(l => l.Likes,
+                 c => c.EQ(u => u.UserName, user.Name));
+            messagesCollection.Update(search, updateQ);
         }
 
         public IEnumerable<Message> GetPopularMessages()
         {
-            //TODO: Здесь нужно возвращать 10 самых популярных сообщений
-            //TODO: Важно сортировку выполнять на сервере
-            //TODO: Тут будет полезен AggregationFramework
-            return Enumerable.Empty<Message>();
+            var or = new BsonDocument()
+            {
+                {
+                    "$or",
+                    new BsonArray
+                    {
+                        new BsonDocument {{"$eq", new BsonArray {"$likes", BsonNull.Value}}},
+                        new BsonDocument {{"$eq", new BsonArray {"$likes", new BsonArray()}}}
+                    }
+                }
+            };
+            var cond1 = new BsonDocument {{"$cond", new BsonArray {or, new BsonArray {BsonNull.Value}, "$likes"}}};
+            var cond2 = new BsonDocument
+            {
+                {
+                    "$cond", new BsonArray
+                    {
+                        or,
+                        new BsonDocument("$add", new BsonArray(new[] {0})),
+                        new BsonDocument("$add", new BsonArray(new[] {1}))
+                    }
+                }
+            };
+            var project = new BsonDocument
+            {
+                {
+                    "$project",
+                    new BsonDocument
+                    {
+                        {"Id", 1},
+                        {"userName", 1},
+                        {"text", 1},
+                        {"createDate", 1},
+                        {"likes", cond1},
+                        {"count", cond2}
+
+                    }
+                }
+            };
+            var unwind = new BsonDocument
+            {
+                {"$unwind", "$likes"}
+            };
+            var group = new BsonDocument
+            {
+                {
+                    "$group",
+                    new BsonDocument
+                    {
+                        {
+                            "_id", new BsonDocument
+                            {
+                                {"_id", "$_id"},
+                                {"userName", "$userName"},
+                                {"text", "$text"},
+                                {"createDate", "$createDate"}
+
+                            }
+                        },
+                        {"Likes", new BsonDocument {{"$sum", "$count"}}}
+
+                    }
+                }
+            };
+            var sort = new BsonDocument { { "$sort", new BsonDocument { { "Likes", -1 } } } };
+            var limit = new BsonDocument { { "$limit", 10 } };
+            var messages = messagesCollection.Aggregate(project, unwind, group, sort, limit).ResultDocuments;
+            return (from item in messages
+                    let m = item["_id"]
+                    let md = BsonSerializer.Deserialize<MessageDocument>((BsonDocument)m)
+                    let likes = (int)item["Likes"]
+                    select
+                        new Message
+                        {
+                            CreateDate = md.CreateDate,
+                            Id = md.Id,
+                            Likes = likes,
+                            Text = md.Text,
+                            User = new User { Name = md.UserName }
+                        }).ToArray();
+
         }
 
         public IEnumerable<UserMessage> GetMessages(User user)
         {
-            //TODO: Здесь нужно получать все сообщения конкретного пользователя
-            return Enumerable.Empty<UserMessage>();
+            var messages =
+                messagesCollection.Find(Query<MessageDocument>.EQ(n => n.UserName, user.Name))
+                    .SetSortOrder(SortBy<MessageDocument>.Descending(d => d.CreateDate));
+            return messages.Select(item => new UserMessage
+            {
+                Id = item.Id,
+                User = new User { Name = item.UserName },
+                Text = item.Text,
+                CreateDate = item.CreateDate,
+                Liked = item.Likes != null && item.Likes.Any(u => u.UserName == user.Name),
+                Likes = item.Likes == null ? 0 : item.Likes.Count()
+            }).ToArray();
         }
     }
 }
